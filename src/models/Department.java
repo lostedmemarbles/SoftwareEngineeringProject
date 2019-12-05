16fe456cb5236929b8cde7f71f83fa23c799dfa1
Paul Lapsley <51719661+no5544554@users.noreply.github.com>
2019-12-05 14:30:12 -0600

    Add files via upload

---
+++ src/models/Department.java
@@ -0,0 +1,11 @@
+package models;
+
+public enum Department {
+	Produce,
+	Deli,
+	Butcher,
+	Bakery,
+	Pop,
+	Snacks,
+	Canned
+}
---
+++ src/models/Markdown.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum Markdown {
+	None,
+	Clearance,
+	Sale,
+	BuyOneGetOneFree
+}
